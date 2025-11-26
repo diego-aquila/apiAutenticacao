8bf6c8a5d3228bb2e54bd5ca3b754e3692f91832
diego-aquila <diego@diegoaquila.com.br>
2025-11-25 21:40:19 -0300

    Implementação da classe authservice para separação de responsabilidade da autenticação

---
+++ Services/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using apiAutenticacao.Data;
+using apiAutenticacao.Models;
+using apiAutenticacao.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static BCrypt.Net.BCrypt;
+
+
+namespace apiAutenticacao.Services
+{
+    public class AuthService
+    {
+
+        private readonly AppDbContext _context;
+
+
+        public AuthService(AppDbContext context) {
+
+            _context = context;
+        
+        }
+
+
+        public async Task<String> Login(LoginDTO dadosUsuario)
+        {
+
+            Usuario? usuarioEncontrado = await _context.Usuarios.
+                FirstOrDefaultAsync(usuario => usuario.Email == dadosUsuario.Email);
+
+            if (usuarioEncontrado != null)
+            {
+                bool isValidPassword = Verify(dadosUsuario.Senha, usuarioEncontrado.Senha);
+
+                if (isValidPassword)
+                {
+                    return "Login realizado com sucesso";
+
+                }
+
+                return "Login não realizado. Email ou senha incorretos";
+
+            }
+
+            return "Usuário não encontrado!";
+
+
+        }
+
+
+
+
+
+
+    }
+}
